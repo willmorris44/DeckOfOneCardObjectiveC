7583829f1b9b777d7d8de5df4ba084034c887f5d
Will Morris <william.morris117@gmail.com>
2019-05-21 14:41:03 -0600

    first commit

---
+++ DeckOfOneCardObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeckOfOneCardObjectiveC
+//
+//  Created by Will morris on 5/21/19.
+//  Copyright © 2019 devmtn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeckOfOneCardObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeckOfOneCardObjectiveC
+//
+//  Created by Will morris on 5/21/19.
+//  Copyright © 2019 devmtn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
